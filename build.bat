7526a13da3d7686931841d1862eb8f84698ecb85
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2023-08-20 15:44:57 +0200

    improve build script

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@ rmdir /s /q dist
 pyinstaller --add-data "assets/icon.ico;assets" -i "assets/icon.ico" --noupx --windowed --onedir mike.py
 del dist\mike\PyQt5\Qt5\bin\opengl32sw.dll
 del dist\mike\PyQt5\Qt5\bin\d3dcompiler_47.dll
+move dist\mike dist\mike-force
